592afadf1f28944f5796175cb4f2bdad769dfdda
Jimmy Hickey <jimmyjhickey@gmail.com>
2018-03-08 17:13:34 -0600

    KMeans clustering prototype

--- py/test.py
+++ py/test.py
@@ -1,9 +1,29 @@
 # import the necessary packages
-import numpy as np
-import argparse
+import pandas as pd
+
+from sklearn.cluster import KMeans
+from sklearn.decomposition import PCA
+
+import plotly.graph_objs as go
+from plotly.offline import iplot
 import cv2
 
-img = cv2.imread('../test_data/data1.png', 0)
-cv2.imshow('image',img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+# open image
+img = cv2.imread('../img_data/data1.png')
+
+df = pd.read_csv('../img_data/data1.csv')
+
+# fit on just BGR values (not x and y)
+X = df.iloc[:, 2:]
+df["cluster"] = KMeans(n_clusters=2).fit_predict(X)
+
+print(df.iloc[200]["x"])
+print(img[200][200])
+
+for i in range(df.shape[0]):
+    if df.iloc[i]["cluster"] == 1:
+        img[df.iloc[i]["x"]][df.iloc[i]['y']] = [0, 0, 0]
+
+
+
+cv2.imwrite('testtest.png', img)
