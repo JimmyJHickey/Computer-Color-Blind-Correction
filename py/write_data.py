655955f9fa56994b4fc29bf4f032ef5bd50b9f9e
Jimmy Hickey <jimmyjhickey@gmail.com>
2018-03-21 09:27:18 -0500

    Added random noise to data generation.

--- py/write_data.py
+++ py/write_data.py
@@ -1,16 +1,38 @@
 import cv2
 import csv
+import numpy as np
+
+
+# Add random noise in the range of [0, 255]
+# If underflow or overflow take min or max respectively
+def noise_with_bounds(data, mean, sd):
+    noise = np.ceil(np.random.normal(mean, sd))
+    if noise + data < 0:
+        return 0
+    elif noise + data > 255:
+        return 255
+    else:
+        return data + noise
 
 # open image
-img = cv2.imread('../img_data/Ishihara.png')
+data_name = "data1"
+img = cv2.imread('../img_data/' + data_name + '.png')
 width = img.shape[0]
 height = img.shape[1]
 
+
 # write to CSV
-with open("../img_data/Ishihara.csv", 'w', newline='') as csv_file:
+with open("../img_data/" + data_name + ".csv", 'w', newline='') as csv_file:
     writer = csv.writer(csv_file, delimiter=',')
     writer.writerow(['x', 'y', 'B', 'G', 'R'])
     for i in range(width):
         for j in range(height):
-            writer.writerow([i, j, img[i, j, 0], img[i, j, 1], img[i, j, 2]])
 
+            # Add normal noise to the data to quell over fitting
+            B = noise_with_bounds(img[i, j, 0], 0, 5)
+            G = noise_with_bounds(img[i, j, 1], 0, 5)
+            R = noise_with_bounds(img[i, j, 2], 0, 5)
+            writer.writerow([i, j, B, G, R])
+            img[i][j] = [B, G, R]
+
+cv2.imwrite('noise_image.png', img)
